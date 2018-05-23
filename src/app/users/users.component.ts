9b73cef2a9aa31cbf9549a64d13187a75e0cb784
aleksandarj93 <aleksandar.jankovic@saga.rs>
2018-05-23 16:07:45 +0200

    user-create paketi

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -16,17 +16,37 @@ import {ErrorStateMatcher} from '@angular/material/core';
 })
 
 export class UsersComponent implements OnInit {
-  // userForm = new FormGroup ({
     firstName = new FormControl('', [ Validators.required ]);
     lastName = new FormControl('', [ Validators.required ]);
     password = new FormControl('', [ Validators.required ]);
-  // });
+    mailQuota = new FormControl();
+    mailMsgQuota = new FormControl();
 
+  languages =
+  [
+    { id: "en", name: "English" },
+    { id: "de", name: "German" },
+    { id: "es", name: "Spanish" },
+    { id: "fr", name: "French" },
+    { id: "ja", name: "Japanese" },
+    { id: "ko", name: "Korean" },
+    { id: "zh-CN", name: "Simplified Chinese" },
+    { id: "zh-TW", name: "Traditional Chinese" }
+  ];
+  selectedLanguage = "en";
 
+  packages =
+  [
+    { 
+      id: "neptune", 
+      name: "neptune",
+      attributes: {"mailMsgMaxBlocks": 800, "mailQuota": -2, "mailMsgQuota": 6000, "mailAllowedServiceAccess": "+imap:ALL$+imaps:ALL$+pop:ALL$+pops:ALL$+smtp:ALL$+http:ALL"}
+    }
+  ];
+  selectedPackage = null;
+  
   matcher = new MyErrorStateMatcher();
 
-
-  user: createJsonMode = new createJsonMode();
   onlyStatus: string;
   message: string;
   IsHidden= true;
@@ -43,22 +63,9 @@ export class UsersComponent implements OnInit {
 
   onSubmit() {
 
-    // this.user.dn = "uid=" + this.userForm.value.firstName + ",ou=People,o=domen1.rs,o=isp"
-    // this.user.attributes.push(new NameValue("Objectclass", [new Value("top"), new Value("person"),  new Value("account"),  new Value("inetOrgPerson")]));
-    // this.user.attributes.push(new NameValue("uid",[new Value(this.userForm.value.firstName)]));
-    // this.user.attributes.push(new NameValue("cn",[new Value(this.userForm.value.firstName)]));
-    // this.user.attributes.push(new NameValue("sn",[new Value(this.userForm.value.lastName)]));
-    // this.user.attributes.push(new NameValue("mail",[new Value(this.userForm.value.firstName + "." + this.userForm.value.lastName + "@example.com")]));
-
     var formResult = {};
     var attributes = [];
     var valuesObjClas = [];
-    var valuesUid = [];
-    var valuesCN = [];
-    var valuesSN = [];
-    var valuesMail = [];
-    var valuesInetUser = [];
-    var valuesUserPassword = [];
 
     valuesObjClas.push({"value": "top"});
     valuesObjClas.push({"value": "person"});
@@ -66,29 +73,70 @@ export class UsersComponent implements OnInit {
     valuesObjClas.push({"value": "inetuser"});
     valuesObjClas.push({"value": "inetOrgPerson"});
     valuesObjClas.push({"value": "ipuser"});
+    valuesObjClas.push({"value": "organizationalperson"});
+    valuesObjClas.push({"value": "iplanetpreferences"});
+    valuesObjClas.push({"value": "daventity"});
+    valuesObjClas.push({"value": "inetLocalMailRecipient"});
+    valuesObjClas.push({"value": "icscalendaruser"});
+    valuesObjClas.push({"value": "inetmailUser"});
     attributes.push({"name": "Objectclass", "values": valuesObjClas});
+
+    var uid = this.firstName.value + "_" + this.lastName.value;
     
-    valuesUid.push({"value": this.firstName.value});
-    attributes.push({"name": "uid", "values": valuesUid});
+    attributes.push({"name": "uid", "values": [{ "value": uid }]});
 
-    valuesCN.push({"value": this.firstName.value});
-    attributes.push({"name": "cn", "values": valuesCN});
+    attributes.push({"name": "givenName", "values": [{"value": this.firstName.value}]}); //first name
 
-    valuesSN.push({"value": this.lastName.value});
-    attributes.push({"name": "sn", "values": valuesSN});
+    attributes.push({"name": "cn", "values": [{"value": this.firstName.value + " " + this.lastName.value}]});
 
-    valuesInetUser.push({"value": "active"});
-    attributes.push({"name": "inetUserStatus", "values": valuesInetUser});
+    attributes.push({"name": "sn", "values": [{"value": this.lastName.value}]});
 
-    valuesUserPassword.push({"value": this.password.value});
-    attributes.push({"name": "userPassword", "values": valuesUserPassword});
+    attributes.push({"name": "inetUserStatus", "values": [{"value": "active"}]});
 
-    valuesMail.push({"value": this.firstName.value + "." + this.lastName.value + "@example.com"});
-    attributes.push({"name": "mail", "values": valuesMail});
+    attributes.push({"name": "userPassword", "values": [{"value": this.password.value}]});
 
-    formResult = {"dn": "uid=" + this.firstName.value + ",ou=People,o=domen1.rs,o=isp", "attributes": attributes};
+    attributes.push({"name": "preferredlanguage", "values": [{"value": this.selectedLanguage}]});
+
+    
 
 
+
+    if(this.selectedPackage != null) {
+      attributes.push({"name": "inetCos", "values": [{"value": this.selectedPackage}]});
+
+      // za E-mail
+      attributes.push({"name": "mail", "values": [{"value": this.firstName.value + "." + this.lastName.value + "@domen1.rs"}]});
+      attributes.push({"name": "mailUserStatus", "values": [{"value": "active"}]});
+
+      attributes.push({"name": "mailHost", "values": [{"value": "ucs7.sun.saga.rs"}]});
+
+      // cekiran local inbox
+      attributes.push({"name": "mailDeliveryOption", "values": [{"value": "mailbox"}]});
+      
+
+      // dodati atribute sa vrednostima iz paketa
+      this.packages.forEach(element => { 
+        if (element.id == this.selectedPackage) {
+          if (this.mailQuota.value != null) { attributes.push({"name": "mailQuota", "values": [{"value": this.mailQuota.value}]}); }
+          else { attributes.push({"name": "mailQuota", "values": [{"value": element.attributes.mailQuota}]}); }
+          
+          if(this.mailMsgQuota.value != null) { attributes.push({"name": "mailMsgQuota", "values": [{"value": this.mailMsgQuota.value}]}); }
+          else { attributes.push({"name": "mailMsgQuota", "values": [{"value": element.attributes.mailMsgQuota}]}); }
+
+          attributes.push({"name": "mailMsgMaxBlocks", "values": [{"value": element.attributes.mailMsgMaxBlocks}]});
+          attributes.push({"name": "mailAllowedServiceAccess", "values": [{"value": element.attributes.mailAllowedServiceAccess}]});
+        }
+      });
+
+
+      // za Kalendar
+      attributes.push({"name": "icsTimezone", "values": [{"value": "Europe/Paris"}]});
+    }
+   
+
+    formResult = {"dn": "uid=" + uid + ",ou=People,o=domen1.rs,o=isp", "attributes": attributes};
+
+    console.log(JSON.stringify(formResult));
 
     this._userService.addUser(formResult)
     .subscribe(
